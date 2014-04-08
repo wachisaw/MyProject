5fb88c5a077b6f6817ec4598c54449a0ddd2f09e
Mac MIS 01 <wachisaw@gmail.com>
2014-04-08 09:48:28 +0700

    ghirtuhgiurthgiurhrth

---
+++ TestEWI/TestEWI/test1.h
@@ -0,0 +1,13 @@
+//
+//  test1.h
+//  TestEWI
+//
+//  Created by MSC on 4/8/14.
+//  Copyright (c) 2014 MSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test1 : UITableViewCell
+
+@end
---
+++ TestEWI/TestEWI/test1.m
@@ -0,0 +1,29 @@
+//
+//  test1.m
+//  TestEWI
+//
+//  Created by MSC on 4/8/14.
+//  Copyright (c) 2014 MSC. All rights reserved.
+//
+
+#import "test1.h"
+
+@implementation test1
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
